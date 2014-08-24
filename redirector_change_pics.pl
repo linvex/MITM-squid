c69db18b4b215e402338e8a2af873c4f2cfcc93d
linvex <1249979379@163.com>
2014-08-24 17:15:03 +0800

    Create redirector_change_pics.pl

---
+++ redirector_change_pics.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+$|=1;
+$count = 0;
+$pid = $$;
+while (<>) {
+	chomp $_;
+	if ($_ =~ /(.*\.jpg)/i) {
+		$url = $1;
+		system("/usr/bin/wget", "-q", "-O","/mydoc/test/images/$pid-$count.jpg", "$url");
+		system("/usr/bin/mogrify", "-flip","/mydoc/test/images/$pid-$count.jpg");
+		system("/usr/bin/convert", "/mydoc/test/images/$pid-$count.jpg", "/mydoc/test/logo.png", "-composite", "/mydoc/test/images/$pid-$count-1.jpg");
+		system("/bin/chmod", "644", "/mydoc/test/images/$pid-$count-1.jpg");
+		print "http://192.168.56.1/images/$pid-$count-1.jpg\n";
+#		print "http://192.168.56.1/images/1.jpg\n";
+	}
+	elsif ($_ =~ /(.*\.gif)/i) {
+		$url = $1;
+		system("/usr/bin/wget", "-q", "-O","/mydoc/test/images/$pid-$count.gif", "$url");
+		system("/usr/bin/mogrify", "-flip","/mydoc/test/images/$pid-$count.gif");
+		system("/bin/chmod", "644", "/mydoc/test/images/$pid-$count.gif");
+		print "http://192.168.56.1/images/$pid-$count.gif\n";
+#		print "http://192.168.56.1/images/1.jpg\n";
+	}
+	elsif ($_ =~ /(.*\.png)/i) {
+		$url = $1;
+		system("/usr/bin/wget", "-q", "-O","/mydoc/test/images/$pid-$count.png", "$url");
+		system("/usr/bin/mogrify", "-flip","/mydoc/test/images/$pid-$count.png");
+		system("/bin/chmod", "644", "/mydoc/test/images/$pid-$count.png");
+		print "http://192.168.56.1/images/$pid-$count.png\n";
+#		print "http://192.168.56.1/images/1.jpg\n";
+	}
+#	elsif ($_ =~ /(.*\.bmp)/i) {
+#		$url = $1;
+#		system("/usr/bin/wget", "-q", "-O","/mydoc/test/images/$pid-$count.bmp", "$url");
+#		system("/usr/bin/mogrify", "-flip","/mydoc/test/images/$pid-$count.bmp");
+#		system("/bin/chmod", "644", "/mydoc/test/images/$pid-$count.bmp");
+#		print "http://192.168.56.1/images/$pid-$count.bmp\n";
+#		print "http://192.168.56.1/images/1.jpg\n";
+#	}
+	else {
+		print "$_\n";;
+	}
+	$count++;
+}
